2c26c3ffc321f9523ef200b213d5447eb297f38e
HrishikeshShinde3 <rushishinde0303@gmail.com>
2023-07-09 10:14:00 +0530

    Small API made

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const http= require('http');
-//const data = require('./data');
+const data = require('./data'); //imported data.js 
+// we make data.js file and put  [({name:'abc',email:'abc@gmail.com'})]
 http.createServer((req,resp)=>{
 resp.writeHead(500,{'Content-Type':'application\json'});
-resp.write(JSON.stringify({name:'abc',email:'abc@gmail.com'}));
+resp.write(JSON.stringify(data));
 resp.end();
 }).listen(5000);
